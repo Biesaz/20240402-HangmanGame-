3733ff8880fd7be8c83072093f5238186b7b0b05
Vygantas Vilius <biesaz@gmail.com>
2024-04-17 21:19:55 +0300

    add Guessing the whole word

--- main.py
+++ main.py
@@ -3,41 +3,56 @@ import random
 import stages
 import words
 
-def clear_screan() -> None:
+def clear_screen() -> None:
     os.system('cls')
-clear_screan()
+
+clear_screen()
 
 attempts = 10
-
 chosen_word = random.choice(words.word_list).lower()
 print(f"The chosen word is: {chosen_word}")
-print("Hello!")
 
-display = []
-
-for i in range(len(chosen_word)):
-    display += "\U0001F913"
+display = ["\U0001FA77" for _ in range(len(chosen_word))]
 print(" ".join(display))
 
-game_over = False
+guessed_letters = []
 
+game_over = False
 while not game_over:
     guess = input("Guess a letter or the full word: ").lower()
-    clear_screan()
+    clear_screen()
 
-    for position in range(len(chosen_word)):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = guess
-    print(" ".join(display).upper())
-    
-    if guess not in chosen_word:
-        attempts -= 1
-        print("Incorrect guess. \nYou have", attempts, "attempts left.")
-        if attempts == 0:
-            game_over = True
-            print("Out of attempts. \nThe word was:", chosen_word)
-    if "\U0001F913" not in display:
+    if guess in guessed_letters:
+        print("You already guessed that letter. Try again.")
+        continue
+
+    if len(guess) == 1:  # Guessing a single letter
+        guessed_letters.append(guess)
+        letter_guessed = False
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = guess
+                letter_guessed = True
+        print(" ".join(display).upper())
+        if not letter_guessed:
+            attempts -= 1
+            print("Incorrect guess. \nYou have", attempts, "attempts left.")  
+    else:  # Guessing the whole word
+        if guess == chosen_word:
+            display = list(chosen_word)
+        else:
+            attempts -= 1
+            print("Incorrect guess. \nYou have", attempts, "attempts left.")
+
+    # print(" ".join(display).upper())
+
+    if "\U0001FA77" not in display:
         game_over = True
         print("Congratulations! You guessed the word:", chosen_word)
-    print(stages.stages[attempts])    
+    
+    if attempts == 0:
+        game_over = True
+        print("Out of attempts. \nThe word was:", chosen_word)
+
+    print(stages.stages[attempts])
