1f81b05f937d81f5fa0dac5014a33ed7989cb2ac
Vygantas Vilius <biesaz@gmail.com>
2024-04-30 20:20:12 +0300

    corect modularization

--- hangman_game.py
+++ hangman_game.py
@@ -1,9 +1,9 @@
 import os
 import random
+import logging
 import stages
 import words
 from typing import List
-import logging
 
 # Clearing the log file before the game starts
 open('hangman.log', 'w').close()
@@ -18,7 +18,7 @@ class HangmanGame:
         self.display: List[str] = ["\U0001FA77" for _ in range(len(self.chosen_word))]
         self.guessed_letters: List[str] = []
         self.game_over: bool = False
-
+        
     def clear_screen(self) -> None:
         os.system('cls')
 
@@ -33,33 +33,52 @@ class HangmanGame:
                 self.display[position] = guess
                 letter_guessed = True
         return letter_guessed
+    
+    def validate_input(self, guess: str) -> bool:
+        if guess in self.guessed_letters:
+            output = "You already guessed that letter. Try again."
+            print(output)
+            logging.info(output)
+            return False
 
+        if len(guess) != 1 and len(guess) != len(self.chosen_word):
+            output = "Please enter a single letter or the full word of correct length."
+            print(output)
+            logging.info(output)
+            return False
+
+        if not guess.isalpha():
+            output = "Please enter only letters."
+            print(output)
+            logging.info(output)
+            return False
+        
+        return True
+    
+    def check_game_state(self) -> None:
+        if "\U0001FA77" not in self.display:
+            self.game_over = True
+            output = "Congratulations! You guessed the word: {}".format(self.chosen_word)
+            print(output)
+            logging.info(output)
+        elif self.attempts == 0:
+            self.game_over = True
+            output = "Out of attempts. \nThe word was: {}".format(self.chosen_word)
+            print(output)
+            logging.info(output)
+    
     def play_game(self) -> None:
         self.clear_screen()
-        # print(f"The chosen word is: {self.chosen_word}") # trinti
         self.display_word()
 
         while not self.game_over:
+            print("\nGuessed Letters:", ", ".join(self.guessed_letters))
+            print("Attempts Remaining:", self.attempts)
             guess = input("Guess a letter or the full word: ").lower()
             logging.info(f"Guessed input: {guess}")
             self.clear_screen()
             
-
-            if guess in self.guessed_letters:
-                output = "You already guessed that letter. Try again."
-                print(output)
-                logging.info(output)
-                continue
-
-            # if len(guess) != 1 or len(guess) != len(self.chosen_word):
-            #     output = "Please enter a single letter or the full word of correct length."
-            #     print(output)
-            #     logging.info(output)
-            #     continue
-            if not guess.isalpha():
-                output = "Please enter only letters."
-                print(output)
-                logging.info(output)
+            if not self.validate_input(guess):
                 continue
 
             if len(guess) == 1:  # Guessing a single letter
@@ -80,20 +99,9 @@ class HangmanGame:
                     print(output)
                     logging.info(output)
 
-            if "\U0001FA77" not in self.display:
-                self.game_over = True
-                output = "Congratulations! You guessed the word: {}".format(self.chosen_word)
-                print(output)
-                logging.info(output)
-            if self.attempts == 0:
-                self.game_over = True
-                output = "Out of attempts. \nThe word was: {}".format(self.chosen_word)
-                print(output)
-                logging.info(output)
+            self.check_game_state()
             print(stages.stages[self.attempts])
-            
 
 if __name__ == "__main__":
     game = HangmanGame()
     game.play_game()
-
